6038972dfd434f754d70958ea8dcaa25ef9e1524
mizza-cz <mizza.cz666@gmail.com>
2021-06-17 15:34:05 +0200

    add hp

---
+++ app/js/main.js
@@ -0,0 +1,39 @@
+$(function(){
+   $('.navbar__menu').on('click', function () {
+      $('.navbar__inner').slideToggle();
+    });
+    $('.nav__link').on('click', function () {
+      $(this).closest('.nav__item').toggleClass('active');
+    });
+    $('.scoarboard__inner').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay:true,
+      infinite:true,
+      prevArrow: '<button class="scoarboard__btn scoarboard__btnprev"><img src="images/svg/arrow-left.svg" alt="" ></button> ',
+      nextArrow: ' <button class="scoarboard__btn scoarboard__btnnext"><img src="images/svg/arrow-right.svg" alt = "" ></button > ',
+      responsive: [
+         {
+            breakpoint: 1400,
+            settings: {
+               slidesToShow: 3,
+            }
+         },
+         {
+            breakpoint: 1100,
+            settings: {
+               slidesToShow: 2,
+            }
+         },
+         {
+            breakpoint: 771,
+            settings: {
+               slidesToShow: 1,
+            }
+         },
+        
+      ]
+
+   });
+// end
+});
